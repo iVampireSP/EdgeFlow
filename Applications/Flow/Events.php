52ebb95971c97c206c6136d17648298298771fe0
iVampireSP.com <im@ivampiresp.com>
2022-04-17 09:55:08 +0800

    update

--- Applications/Flow/Events.php
+++ Applications/Flow/Events.php
@@ -210,11 +210,11 @@ class Events
      */
     public static function onClose($client_id)
     {
-        $server = Server::where('client_id', $client_id)->first();
+        $server = Server::where('client_id', $client_id);
 
-        $server->query()->update(['status' => 'offline', 'client_id' => null]);
+        $server->update(['status' => 'offline', 'client_id' => null]);
 
-        echo '服务器离线: ' . $server->name . PHP_EOL;
+        echo '服务器离线: ' . $server->first()->name . PHP_EOL;
         // 向所有人发送 
         //    GateWay::sendToAll("$client_id logout\r\n");
     }
